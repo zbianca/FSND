db779bf98030350dee6f1ca2550a57676535efa7
Bianca Zanardi <bianca.zanardi@gmail.com>
2021-06-11 23:19:01 +0200

    feat: add properties and relationships to database models  Artist, Artist_Genres, Genre, Show, Venue, Venue_Genres

--- projects/01_fyyur/src/app.py
+++ projects/01_fyyur/src/app.py
@@ -39,31 +39,59 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    status = db.Column(db.String(120))
+    genres = db.relationship('Venue_Genres', backref='venue')
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    status = db.Column(db.String(120))
+    genres = db.relationship('Artist_Genres', backref='artist')
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+
+venueGenres = db.Table('Venue_Genres',
+                       db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), nullable=False),
+                       db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+                       )
+
+artistGenres = db.Table('Artist_Genres',
+                        db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), nullable=False),
+                        db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+                        )
+
 
 #----------------------------------------------------------------------------#
 # Filters.
