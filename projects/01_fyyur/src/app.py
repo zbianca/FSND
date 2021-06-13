9dc48054bb4caa6907c499f4793f12c614cf0d08
Bianca Zanardi <bianca.zanardi@gmail.com>
2021-06-13 20:53:57 +0200

    refactor: make venue address required in db

--- projects/01_fyyur/src/app.py
+++ projects/01_fyyur/src/app.py
@@ -53,7 +53,7 @@ class Venue(db.Model):
     name = db.Column(db.String, nullable=False)
     city = db.Column(db.String(120), nullable=False)
     state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120))
+    address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
