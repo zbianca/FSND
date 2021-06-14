10264a2ec4d6d4b0a644bdc2d87fcdf45cde2f88
Bianca Zanardi <bianca.zanardi@gmail.com>
2021-06-14 23:55:05 +0200

    feat: populate edit forms  venue and artist

--- projects/01_fyyur/src/app.py
+++ projects/01_fyyur/src/app.py
@@ -13,6 +13,7 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
+import sys
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -375,22 +376,30 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
-  return render_template('forms/edit_artist.html', form=form, artist=artist)
+    form = ArtistForm(meta={'csrf': False})
+    statusBool = False
+    query = Artist.query.get_or_404(artist_id)
+
+    if query.status:
+        statusBool = True
+    form.name.data = query.name
+    form.genres.default = query.genres
+    form.city.data = query.city
+    form.state.data = query.state
+    form.phone.data = query.phone
+    form.website_link.data = query.website_link
+    form.facebook_link.data = query.facebook_link
+    form.seeking_venue.data = statusBool
+    form.seeking_description.data = query.status
+    form.image_link.data = query.image_link
+
+    artist = {
+        "id": artist_id,
+        "name": query.name
+    }
+
+    return render_template('forms/edit_artist.html', form=form, artist=artist)
+
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
@@ -399,25 +408,35 @@ def edit_artist_submission(artist_id):
 
   return redirect(url_for('show_artist', artist_id=artist_id))
 
+
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
-  return render_template('forms/edit_venue.html', form=form, venue=venue)
+    form = VenueForm(meta={'csrf': False})
+
+    statusBool = False
+    query = Venue.query.get_or_404(venue_id)
+    if query.status:
+        statusBool = True
+
+    form.name.data = query.name
+    form.genres.default = query.genres
+    form.address.data = query.address
+    form.city.data = query.city
+    form.state.data = query.state
+    form.phone.data = query.phone
+    form.website_link.data = query.website_link
+    form.facebook_link.data = query.facebook_link
+    form.seeking_talent.data = statusBool
+    form.seeking_description.data = query.status
+    form.image_link.data = query.image_link
+
+    venue = {
+        "id": venue_id,
+        "name": query.name
+    }
+
+    return render_template('forms/edit_venue.html', form=form, venue=venue)
+
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
