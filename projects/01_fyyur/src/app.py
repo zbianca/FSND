81bd40bfad6ad6e695860439a633b37e424aa530
Bianca Zanardi <bianca.zanardi@gmail.com>
2021-06-15 14:29:17 +0200

    refactor: move models to own file

--- projects/01_fyyur/src/app.py
+++ projects/01_fyyur/src/app.py
@@ -13,10 +13,12 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
+from models import *
+import sys
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
-
 
 def create_app(config):
     _app = Flask(__name__)
@@ -31,80 +33,6 @@ csrf = CSRFProtect()
 app = create_app('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-venueGenres = db.Table('Venue_Genres',
-                       db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), nullable=False),
-                       db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-                       )
-
-artistGenres = db.Table('Artist_Genres',
-                        db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), nullable=False),
-                        db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-                        )
-
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    status = db.Column(db.String(280))
-    genres = db.relationship('Genre', secondary=venueGenres, lazy='subquery', backref='venue')
-
-    def __repr__(self):
-        return f'<Venue ID: {self.id}, genres: {self.genres}>'
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    status = db.Column(db.String(280))
-    genres = db.relationship('Genre', secondary=artistGenres, lazy='subquery', backref='artist')
-
-    def __repr__(self):
-        return f'<Artist ID: {self.id}, genres: {self.genres}>'
-
-
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    artist = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    venue = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return f'<Show ID: {self.id}, artist: {self.artist}, venue: {self.venue}, date: {self.date}>'
-
-
-class Genre(db.Model):
-    __tablename__ = 'Genre'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-
-    def __repr__(self):
-        return f'<Genre name: {self.name}>'
 
 
 #----------------------------------------------------------------------------#
@@ -227,7 +155,7 @@ def show_venue(venue_id):
 
 @app.route('/venues/create', methods=['GET'])
 def create_venue_form():
-    form = VenueForm()
+    form = VenueForm(meta={'csrf': False})
     return render_template('forms/new_venue.html', form=form)
 
 
@@ -262,6 +190,7 @@ def create_venue_submission():
                 errorMessage += error + " "
             flash(errorMessage)
     except:
+        Raise
         db.session.rollback()
         flash('An error occurred. Venue ' + form.name.data + ' could not be listed.')
     finally:
@@ -505,7 +434,6 @@ def edit_venue_submission(venue_id):
                 errorMessage += error + " "
             flash(errorMessage)
     except:
-        Raise
         db.session.rollback()
         flash('An error occurred. Venue ' + form.name.data + ' could not be updated.')
     finally:
@@ -518,7 +446,7 @@ def edit_venue_submission(venue_id):
 
 @app.route('/artists/create', methods=['GET'])
 def create_artist_form():
-    form = ArtistForm()
+    form = ArtistForm(meta={'csrf': False})
     return render_template('forms/new_artist.html', form=form)
 
 @app.route('/artists/create', methods=['POST'])
@@ -581,8 +509,7 @@ def shows():
 
 @app.route('/shows/create')
 def create_shows():
-  # renders form. do not touch.
-  form = ShowForm()
+  form = ShowForm(meta={'csrf': False})
   return render_template('forms/new_show.html', form=form)
 
 
